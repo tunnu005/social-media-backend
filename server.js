eb26796eb73298a609eeec46b6482599cb369ec4
tunnu005 <tunnuthe005@gmail.com>
2024-11-10 11:31:35 +0530

    domain1

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(cors({
     origin: [
         'http://localhost:5174',
         'http://localhost:5173',
-        'https://social-media-frontend-eight-theta.vercel.app'
+        'https://buzzzy.vercel.app'
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
@@ -47,7 +47,7 @@ const io = new SocketIOServer(server, { // Initialize Socket.IO with HTTP server
         origin: [
             'http://localhost:5174',
             'http://localhost:5173',
-            'https://buzzzy.vercel.app'
+            'https://buzzzy.vercel.app',
         ],
         credentials: true,
     },
