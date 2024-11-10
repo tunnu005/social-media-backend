ff423995d4e2c6a1703e459cbb9d8385b7107cd1
tunnu005 <tunnuthe005@gmail.com>
2024-11-11 00:35:29 +0530

    remove rate

--- server.js
+++ server.js
@@ -22,11 +22,11 @@ const app = express();
 
 // Security Enhancements
 app.use(helmet()); // Sets various HTTP headers to protect your app
-app.use(rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // limit each IP to 100 requests per windowMs
-    message: "Too many requests, please try again later.",
-}));
+// app.use(rateLimit({
+//     windowMs: 15 * 60 * 1000, // 15 minutes
+//     max: 100, // limit each IP to 100 requests per windowMs
+//     message: "Too many requests, please try again later.",
+// }));
 
 // JSON and Cookie Parsing
 app.use(express.json({ limit: '10mb' }));
